5761bfdec8d0b3daaec1f9c159e0015a63775c5a
Siddu R K <Siddu R K@DESKTOP-A48OUSS>
2021-12-11 15:16:14 +0530

    gggg

---
+++ Sample_Maven_Project/src/test/java/Test/Sample_Maven_Project/TC01Test.java
@@ -0,0 +1,16 @@
+package Test.Sample_Maven_Project;
+
+import org.testng.annotations.Test;
+
+public class TC01Test {
+	@Test
+	public void webshop() {
+		System.out.println("Running Webshop");	
+	}
+	@Test
+	public void medicalshop() {
+		System.out.println("Running medical shop");
+	}
+}
+
+
---
+++ Sample_Maven_Project/src/test/java/Test/Sample_Maven_Project/TC02Test.java
@@ -0,0 +1,14 @@
+package Test.Sample_Maven_Project;
+
+import org.testng.annotations.Test;
+
+public class TC02Test {
+	@Test
+	public void panshop() {
+		System.out.println("Running panshop");
+	}
+	@Test
+	public void wineshop() {
+		System.out.println("Running wineshop");
+	}
+}
